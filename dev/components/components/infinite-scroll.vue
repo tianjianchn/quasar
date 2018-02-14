c6f0a9a0351ced330bac2f5d41d3a35b2c9407c1
tianjianchn <tianjianchn@qq.com>
2018-02-14 15:21:06 +0800

    Update: make infinite-scroll more intelligent and support immediate prop

--- dev/components/components/infinite-scroll.vue
+++ dev/components/components/infinite-scroll.vue
@@ -5,15 +5,15 @@
 
       <br>
       <q-infinite-scroll :handler="refresher">
-        <p v-for="(item, index) in items" class="caption">
+        <p v-for="(item, index) in items" :key="index" class="caption">
           <q-chip square color="secondary" class="shadow-1">
             {{ index + 1 }}
           </q-chip>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
         </p>
 
-        <div class="row justify-center" style="margin-bottom: 50px;">
-          <q-spinner name="dots" slot="message" :size="40"></q-spinner>
+        <div class="row justify-center" slot="message" style="margin-bottom: 50px;">
+          <q-spinner name="dots" :size="40"></q-spinner>
         </div>
       </q-infinite-scroll>
     </div>
@@ -24,21 +24,24 @@
 export default {
   data () {
     return {
-      items: [{}, {}, {}, {}, {}]
+      items: []
     }
   },
   methods: {
     refresher (index, done) {
+      // console.log('load more ' + index)
       setTimeout(() => {
         let items = []
 
-        for (let i = 0; i < 7; i++) {
-          items.push({})
+        if (this.items.length < 7) {
+          for (let i = 0; i < 1; i++) {
+            items.push({})
+          }
         }
 
         this.items = this.items.concat(items)
         done()
-      }, 2500)
+      }, 1500)
     }
   }
 }
